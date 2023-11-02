fbb8a0a7a40e2b8e29a94b1d08661ed295d8367f
Yasas Lowe <yasas@mintpay.lk>
2023-11-02 12:10:37 +0530

    channel view

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -17,10 +17,10 @@ interface CHANNEL {
 
 const Dashboard = () => {
 
-    const state1: {
-        series: ApexOptions['series'],
-        options: ApexOptions
-    } = {
+    const [state1, setState1] = useState<{
+        series: ApexCharts.ApexOptions["series"];
+        options: ApexCharts.ApexOptions
+    }>({
         series: [{
             name: "Percentage",
             data: [13, 3, 9, 43, 8, 6, 9, 1, 14]
@@ -28,7 +28,7 @@ const Dashboard = () => {
         options: {
             chart: {
                 height: 350,
-                type: 'line',
+                type: "line",
                 zoom: {
                     enabled: false
                 }
@@ -37,30 +37,30 @@ const Dashboard = () => {
                 enabled: false
             },
             stroke: {
-                curve: 'straight'
+                curve: "straight"
             },
             title: {
-                text: 'Hate speech Probability over videos',
-                align: 'left'
+                text: "Hate speech Probability over videos",
+                align: "left"
             },
             grid: {
                 row: {
-                    colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+                    colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
                     opacity: 0.5
                 },
             },
             xaxis: {
-                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+                categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep"],
             }
         },
 
 
-    };
+    });
 
-    const state2: {
-        series: ApexOptions['series'],
-        options: ApexOptions
-    } = {
+    const [state2, setState2] = useState<{
+        series: ApexCharts.ApexOptions["series"];
+        options: ApexCharts.ApexOptions
+    }>({
 
         series: [{
             name: "Percentage",
@@ -69,7 +69,7 @@ const Dashboard = () => {
         options: {
             chart: {
                 height: 350,
-                type: 'line',
+                type: "line",
                 zoom: {
                     enabled: false
                 }
@@ -78,29 +78,29 @@ const Dashboard = () => {
                 enabled: false
             },
             stroke: {
-                curve: 'straight'
+                curve: "straight"
             },
             title: {
-                text: 'Deepfake Probability over videos',
-                align: 'left'
+                text: "Deepfake Probability over videos",
+                align: "left"
             },
             grid: {
                 row: {
-                    colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+                    colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
                     opacity: 0.5
                 },
             },
             xaxis: {
-                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+                categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep"],
             }
         },
 
 
-    };
-    const state3: {
-        series: ApexOptions['series'],
-        options: ApexOptions
-    } = {
+    });
+    const [state3, setState3] = useState<{
+        series: ApexCharts.ApexOptions["series"];
+        options: ApexCharts.ApexOptions
+    }>({
 
         series: [{
             name: "Percentage",
@@ -109,7 +109,7 @@ const Dashboard = () => {
         options: {
             chart: {
                 height: 350,
-                type: 'line',
+                type: "line",
                 zoom: {
                     enabled: false
                 }
@@ -118,29 +118,29 @@ const Dashboard = () => {
                 enabled: false
             },
             stroke: {
-                curve: 'straight'
+                curve: "straight"
             },
             title: {
-                text: 'Audio Similarity over videos',
-                align: 'left'
+                text: "Audio Similarity over videos",
+                align: "left"
             },
             grid: {
                 row: {
-                    colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+                    colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
                     opacity: 0.5
                 },
             },
             xaxis: {
-                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+                categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep"],
             }
         },
 
 
-    };
-    const state4: {
-        series: ApexOptions['series'],
-        options: ApexOptions
-    } = {
+    });
+    const [state4, setState4] = useState<{
+        series: ApexCharts.ApexOptions["series"];
+        options: ApexCharts.ApexOptions
+    }>({
 
         series: [{
             name: "Percentage",
@@ -149,7 +149,7 @@ const Dashboard = () => {
         options: {
             chart: {
                 height: 350,
-                type: 'line',
+                type: "line",
                 zoom: {
                     enabled: false
                 }
@@ -158,35 +158,35 @@ const Dashboard = () => {
                 enabled: false
             },
             stroke: {
-                curve: 'straight'
+                curve: "straight"
             },
             title: {
-                text: 'Video Similarity over videos',
-                align: 'left'
+                text: "Video Similarity over videos",
+                align: "left"
             },
             grid: {
                 row: {
-                    colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+                    colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
                     opacity: 0.5
                 },
             },
             xaxis: {
-                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+                categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep"],
             }
         },
-    };
+    });
 
-    const [selectedChannel, setSelectedChannel] = useState('')
+    const [selectedChannel, setSelectedChannel] = useState<CHANNEL | null>(null)
     const [channels, setChannels] = useState<CHANNEL[]>([])
 
     useEffect(() => {
-        getChannelList()
+        getOwnedChannelList()
     }, []);
 
 
     const [ownedChannels, setOwnedChannels] = useState<string[]>([])
 
-    const getChannelList = () => {
+    const getOwnedChannelList = () => {
         const emailID = localStorage.getItem('email').split('@')[0]
 
         if (emailID === null) {
@@ -205,32 +205,38 @@ const Dashboard = () => {
             } else {
                 console.log("No data available");
             }
-
         })
+    }
 
+    useEffect(() => {
+        getChannelList()
+    }, [ownedChannels])
+
+    const getChannelList = () => {
 
         const channelRef = dbRef(firebaseDatabase, 'channels/');
 
         onValue(channelRef, (snapshot) => {
             const data = snapshot.val();
             if (snapshot.exists()) {
+                const tempChannels: CHANNEL[] = []; // Temporary array to hold the channels
 
+                setChannels([])
 
-                const channelListMaps: CHANNEL[] = Object.keys(snapshot.val()).map((key, index): CHANNEL => {
+                Object.keys(data).map((key, index) => {
                     const convertingChannel: CHANNEL = {
                         name: key,
-                        ready: snapshot.val()[key].ready,
-                        data: snapshot.val()[key].data,
+                        ready: data[key].ready,
+                        data: data[key].data,
                     } as CHANNEL
 
                     if (ownedChannels.includes(convertingChannel.name)) {
-                        return convertingChannel;
+                        tempChannels.push(convertingChannel); // Push to the temporary array
                     }
 
+
                 })
-
-                setChannels(channelListMaps)
-
+                setChannels(tempChannels);
 
             } else {
 
@@ -239,15 +245,70 @@ const Dashboard = () => {
     }
 
 
+    // useEffect that updates the chart states based on selectedChannel
+    useEffect(() => {
+        if (selectedChannel) {
+            setState1(prevState => ({
+                ...prevState,
+                series: [selectedChannel.data[0]],
+                options: {
+                    ...prevState.options,
+                    title: {
+                        ...prevState.options.title,
+                        text: selectedChannel.data[0].name + ' over videos'
+                    }
+                }
+            }));
+            setState2(prevState => ({
+                ...prevState,
+                series: [selectedChannel.data[1]],
+                options: {
+                    ...prevState.options,
+                    title: {
+                        ...prevState.options.title,
+                        text: selectedChannel.data[1].name + ' over videos'
+                    }
+                }
+            }));
+            setState3(prevState => ({
+                ...prevState,
+                series: [selectedChannel.data[2]],
+                options: {
+                    ...prevState.options,
+                    title: {
+                        ...prevState.options.title,
+                        text: selectedChannel.data[2].name + ' over videos'
+                    }
+                }
+            }));
+            setState4(prevState => ({
+                ...prevState,
+                series: [selectedChannel.data[3]],
+                options: {
+                    ...prevState.options,
+                    title: {
+                        ...prevState.options.title,
+                        text: selectedChannel.data[3].name + ' over videos'
+                    }
+                }
+            }));
+        }
+    }, [selectedChannel]);
+
+
     return (
         <div>
             <>
                 <header className="bg-white shadow">
-                    <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 flex">
+                    <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 flex space-x-1">
                         {channels.map((channel, index) => {
-                            return <span key={index}
-                                         className="cursor-pointer inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
-                                {/*{channel.name}*/}
+                            return <span
+                                onClick={() => {
+                                    setSelectedChannel(channel)
+                                }}
+                                key={index}
+                                         className=" cursor-pointer inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
+                                {channel.name}
                             </span>
                         })}
                     </div>
@@ -255,11 +316,10 @@ const Dashboard = () => {
             </>
 
             <div className={`grid grid-cols-1 grid-rows-4 sm:grid-cols-2 sm:grid-rows-2 pt-10 gap-2`}>
-                <ReactApexChart
-                    options={state1.options} series={state1.series} type="line" height={350}/>
-                <ReactApexChart options={state2.options} series={state2.series} type="line" height={350}/>
-                <ReactApexChart options={state3.options} series={state3.series} type="line" height={350}/>
-                <ReactApexChart options={state4.options} series={state4.series} type="line" height={350}/>
+                <ReactApexChart options={state1.options} series={state1.series} type="line" height={350} />
+                <ReactApexChart options={state2.options} series={state2.series} type="line" height={350} />
+                <ReactApexChart options={state3.options} series={state3.series} type="line" height={350} />
+                <ReactApexChart options={state4.options} series={state4.series} type="line" height={350} />
             </div>
         </div>
     );
